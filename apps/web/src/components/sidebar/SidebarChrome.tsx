b6e78db695bd4c63b991455edfe2ca2ab6949af8
NateWeav <38162256+NateWeav@users.noreply.github.com>
2026-09-16 19:03:39 +0000

    fix(web): mount Hermes cron notifications

--- apps/web/src/components/sidebar/SidebarChrome.tsx
+++ apps/web/src/components/sidebar/SidebarChrome.tsx
@@ -29,6 +29,7 @@ import { readPullRequestListPreferences } from "../pullRequest/pullRequestListPr
 import { SidebarProviderUpdatePill } from "./SidebarProviderUpdatePill";
 import { SidebarUpdateArchitectureWarning, SidebarUpdatePill } from "./SidebarUpdatePill";
 import { PullRequestGlyph } from "~/components/pullRequest/pullRequestIcons";
+import { HermesCronWatcher } from "../hermes/HermesCronWatcher";
 
 export const SidebarChromeHeader = memo(function SidebarChromeHeader({
   isElectron,
@@ -221,6 +222,7 @@ export const SidebarUtilityMenu = memo(function SidebarUtilityMenu() {
 export const SidebarChromeFooter = memo(function SidebarChromeFooter() {
   return (
     <SidebarFooter className="px-[var(--sidebar-content-inset)] py-1">
+      <HermesCronWatcher />
       <SidebarProviderUpdatePill />
       <SidebarUpdateArchitectureWarning />
       <SidebarUtilityMenu />
